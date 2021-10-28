c12fa0a096107bbcdbf0647dee4b936fe160ac2f
vanhopld98 <vanhopld98@gmail.com>
2021-10-28 10:50:38 +0700

    first commit

---
+++ src/main/java/com/codegym/sercurity/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.codegym.sercurity;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
